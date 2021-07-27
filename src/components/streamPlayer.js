a10892895529d1c79ec666f6a93f5be2a6e85d16
park hyeong gil <gudrlf7913@naver.com>
2021-07-27 10:37:30 +0900

    채팅, 화상 화면 컴포넌트 분리

---
+++ src/components/streamPlayer.js
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from 'react'
+
+export default function StreamPlayer(props) {
+    const container = useRef(null)
+    useEffect(() => {
+        if (!container.current) return;
+        props.videoTrack?.play(container.current);
+
+        return () => {
+            props.videoTrack?.stop();
+        };
+    }, [container, props.videoTrack])
+    useEffect(() => {
+        props.audioTrack?.play();
+
+        return () => {
+            props.audioTrack?.stop();
+        };
+    }, [props.audioTrack])
+
+    return (
+        <>
+            <div ref={container} style={{ width: "480px", height: "320px"}}>
+                    
+            </div>
+        </>
+    )
+}
