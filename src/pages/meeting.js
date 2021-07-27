390232194e7cc7f0754c61ba77b1258b02ce55f8
park hyeong gil <gudrlf7913@naver.com>
2021-07-27 14:48:25 +0900

    채널 화면에 채팅 컴포넌트 추가

--- src/pages/meeting.js
+++ src/pages/meeting.js
@@ -6,9 +6,10 @@ import StreamPlayer from '../components/streamPlayer';
 import AgoraRTC from 'agora-rtc-sdk-ng';
 import RTCClient from '../rtc-client';
 import { IonIcon } from '@ionic/react'
-import { clipboardOutline, peopleOutline, chatboxEllipsesOutline, gridOutline, settingsOutline, micOutline, videocamOutline, shareOutline, logOutOutline, micOffOutline } from 'ionicons/icons'
+import { clipboardOutline, peopleOutline, chatboxEllipsesOutline, gridOutline, settingsOutline, micOutline, micOffOutline, videocamOutline, videocamOffOutline, shareOutline, logOutOutline } from 'ionicons/icons'
 import '../assets/css/channel.css';
 import Chatting from '../components/chatting';
+import useRouter from '../utils/use-router';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -46,7 +47,9 @@ const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' });
 
 const Room = () => {
   const classes = useStyles();
-  const [useMic, setUseMic] = useState(true)
+  const routerCtx = useRouter();
+  const [useMic, setUseMic] = useState(false)
+  const [useVideocam, setUseVideocam] = useState(false)
 
   const {
     localAudioTrack, localVideoTrack, leave, join, joinState, remoteUsers, channelName, userName
@@ -54,6 +57,17 @@ const Room = () => {
 
   const onMic = () => {
     setUseMic(!useMic)
+    localAudioTrack.setEnabled(useMic)
+  }
+
+  const onVideocam = () => {
+    setUseVideocam(!useVideocam)
+    localVideoTrack.setEnabled(useVideocam)
+  }
+
+  const onLeaveChannel = () => {
+    leave();
+    routerCtx.history.push({ pathname: '/' })
   }
 
   return (
@@ -107,16 +121,16 @@ const Room = () => {
             </div>
             <div class="bottom_container">
                 <div class="view_mic">
-                  {useMic ? <IonIcon icon={micOutline} onClick={onMic} /> : <IonIcon icon={micOffOutline} onClick={onMic} /> }
+                  {!useMic ? <IonIcon icon={micOutline} onClick={onMic} /> : <IonIcon icon={micOffOutline} onClick={onMic} /> }
                 </div>
                 <div class="view_video">
-                    <IonIcon icon={videocamOutline} />
+                  {!useVideocam ? <IonIcon icon={videocamOutline} onClick={onVideocam} /> : <IonIcon icon={videocamOffOutline} onClick={onVideocam} /> }
                 </div>
                 <div class="view_share">
                     <IonIcon icon={shareOutline} />
                 </div>
                 <div class="view_out">
-                    <IonIcon icon={logOutOutline} />
+                    <IonIcon icon={logOutOutline} onClick={onLeaveChannel} />
                 </div>
             </div>
         </div>
