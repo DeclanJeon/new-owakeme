ccf1a136b4d70a7cd4a6334ecc044b1e8c76af44
park hyeong gil <gudrlf7913@naver.com>
2021-08-05 16:04:30 +0900

    디바이스 설정 화면

--- src/subcomponents/settingDevice.js
+++ src/subcomponents/settingDevice.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
 import { Select, MenuItem, InputLabel, FormControl } from '@material-ui/core';
+import { setCameraId, setAudioId } from '../reducer/actions/deviceList';
+
+const useStyle = makeStyles({
+    deviceSection: {
+        width: '30vw',
+        height: '100%'
+      },
+})
 
 const SettingDevice = () => {
-    const devices = useSelector(state => state.deviceReducer.deviceList)
+    const classes = useStyle();
+    const devices = useSelector(state => state.deviceReducer.deviceList);
+    const dispatch = useDispatch();
     
+    const selectVideo = useCallback((e) => {
+        dispatch(setCameraId(e.value))
+    }, [])
+
+    const selectAudio = useCallback((e) => {
+        dispatch(setAudioId(e.value))
+    }, [])
+
     return (
         <div>
-            <FormControl>
+            <FormControl className={classes.deviceSection}>
                 <InputLabel>Video</InputLabel>
-                <Select>
-                    {devices.map((obj, index) => {
-                        return <MenuItem value={index}>{obj.label}</MenuItem>
+                <Select
+                    onChange={selectVideo}
+                >
+                    {devices.map((obj) => {
+                        if(obj.kind === 'videoinput'){
+                            return <MenuItem value={obj.deviceId} key={obj.deviceId} >{obj.deviceId}</MenuItem>
+                        }
+                    })}
+                </Select>
+            </FormControl>
+            <FormControl className={classes.deviceSection}>
+                <InputLabel>Audio</InputLabel>
+                <Select
+                    onChange={selectAudio}
+                >
+                    {devices.map((obj) => {
+                        if(obj.kind === 'audioinput'){
+                            return <MenuItem value={obj.deviceId} key={obj.deviceId} >{obj.label}</MenuItem>
+                        }
                     })}
                 </Select>
             </FormControl>
