8cc6479719f0f49268b711e288ad22a0de167969
park hyeong gil <gudrlf7913@naver.com>
2021-08-05 16:02:04 +0900

    불필요 태그 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,9 +82,6 @@ export default function SignIn() {
                     <div id="Create_Channel">
                         <Button style={{ width: '35vw', height: '5vh' }} onClick={onEnterChanel}>Create Channel</Button>
                     </div>
-                    <div id="Channel_ID_URL">
-                        <span>Channel ID or URL</span>
-                    </div>
                 </div>
 
                 <GoogleLoginForm />
