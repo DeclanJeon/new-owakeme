9ee35cd38e37f935337becd9100150cef36a27f4
park hyeong gil <gudrlf7913@naver.com>
2021-08-09 13:45:29 +0900

    navBar 코드 수정

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ChannelUserList from '../subcomponents/channelUserList';
 import Chatting from '../subcomponents/chatting';
 import SettingDevice from '../subcomponents/settingDevice';
 import { IonIcon } from '@ionic/react'
-import { peopleOutline, chatboxEllipsesOutline, gridOutline, settingsOutline } from 'ionicons/icons';
+import { peopleOutline, chatboxEllipsesOutline, settingsOutline } from 'ionicons/icons';
 
 const NavBar = () => {
+    const [selectNavBar, setSelectNavBar] = useState('')
 
     return (
         <div className="row_nav_container">
             <div className="navigaitner">
                 <div id="connect_user_list">
-                    <IonIcon icon={peopleOutline} />
+                    <IonIcon icon={peopleOutline} onClick={() => setSelectNavBar('List')} />
                 </div>
                 <div id="chat">
-                    <IonIcon icon={chatboxEllipsesOutline} />
-                </div>
-                <div id="view_grid">
-                    <IonIcon icon={gridOutline} />
+                    <IonIcon icon={chatboxEllipsesOutline} onClick={() => setSelectNavBar('Chat')} />
                 </div>
                 <div id="setting">
-                    <IonIcon icon={settingsOutline} />
+                    <IonIcon icon={settingsOutline} onClick={() => setSelectNavBar('Setting')} />
                 </div>
             </div>
             
-            {/* <ChannelUserList /> */}
+            {selectNavBar === 'List' ? <ChannelUserList /> : <></>}
                 
-            {/* <Chatting /> */}
+            {selectNavBar === 'Chat' ? <Chatting /> : <></>}
 
-            <SettingDevice />
-            
+            {selectNavBar === 'Setting' ? <SettingDevice /> : <></>}
         </div>
     )
 }
