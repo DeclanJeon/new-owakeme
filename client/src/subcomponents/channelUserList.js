299629fa907bc857706a5bf8627935073715f4da
park hyeong gil <gudrlf7913@naver.com>
2021-08-19 15:04:44 +0900

    사용자 리스트 컴포넌트 코드 작성

--- client/src/subcomponents/channelUserList.js
+++ client/src/subcomponents/channelUserList.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
+import axios from 'axios';
 
 function ChannelUserList() {
-    const userNameList = useSelector(state => state.userReducer.userNameList);
+    const [userNameList, setUserNameList] = useState([]);
+    const channelName = useSelector(state => state.channelReducer.channelName);
+    const { googleLogin, googleLogOut, isLogin, isLogout } = useSelector(state => state.userReducer);
+
+    useEffect(() => {
+        const param = {
+            channelName: channelName
+        }
+        
+        axios.post('/api/get/roomUserName', param).then((res) => {
+            if(res.data.success === true){
+                setUserNameList(res.data.userList);
+            }else{
+                alert(res.data.err);
+            }
+        })
+    }, [{ googleLogin, googleLogOut, isLogin, isLogout }])
     
     return (
         <>
             {userNameList.map((userName, index) => {
                 return (
-                        <div  kye={index}>
+                        <div key={index}>
                             <ListItemAvatar align="left">
                                 <Avatar>
                                     {userName}
