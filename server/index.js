da075ba65b246ca2ef9dce572cdf0b4d12a23499
park hyeong gil <gudrlf7913@naver.com>
2021-08-19 15:06:02 +0900

    사용자 등록, 불러오기 ,삭제 코드 추가

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const firebase = require('firebase/app');
 const firebaseConfig = require('./config/firebaseConfig.js');
 const express = require('express');
+const router = express.Router();
 const bodyParder = require('body-parser');
 const redis = require('redis');
 
@@ -30,6 +31,47 @@ db.collection("RoomList").add({
 }).then((e) => console.log("저장완료 : " + e))
 .catch((err) => console.log("저장 실패 : " + err))*/
 
+app.post('/api/save/roomUserName', (req, res) =>{
+    const bodyData = req.body;
+
+    client.sadd(bodyData.channelName,bodyData.userName);
+    return res.status(200).json({
+        success: true
+    })
+})
+
+app.post('/api/get/roomUserName', (req, res) => {
+    const bodyData = req.body;
+
+    client.smembers(bodyData.channelName, (error, items) => {
+        if(error){
+            return res.status(500).json({
+                success: false,
+                err: error
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            userList: items
+        })
+    });
+})
+
+app.post('/api/remove/roomUserName', (req, res) => {
+    const bodyData = req.body;
+
+    if(client.srem(bodyData.channelName,bodyData.userName)){
+        return res.status(200).json({
+            success: true
+        })
+    }else{
+        return res.status(500).json({
+            success: false
+        })
+    }
+    
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>  console.log(`app listening on port ${port}!!`))
