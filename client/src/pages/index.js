dbdb21aea3df62de08ad4c23bc382513979a8a54
park hyeong gil <gudrlf7913@naver.com>
2021-08-19 15:01:10 +0900

    구글 로그인 성공시 발생하는 함수 코드 작성 및 axios 연동

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -7,8 +7,10 @@ import { Button } from '@material-ui/core';
 import GoogleLoginForm from '../components/googleLoginForm';
 import { userLogIn } from '../reducer/actions/user';
 import { channelEnter } from '../reducer/actions/channel';
-import { setDeviceList } from '../reducer/actions/deviceList'
-import AgoraRTC from 'agora-rtc-sdk-ng'
+import { setDeviceList } from '../reducer/actions/deviceList';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import axios from 'axios';
+import { googleLogIn } from '../reducer/actions/user';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -44,22 +46,58 @@ export default function SignIn() {
   const [userName, setUserName] = useState('')
 
   const onChanelName = useCallback((e) => {
-    e.preventDefault();
-
     setChannelName(e.currentTarget.value)
   },[channelName])
 
   const onUserName = useCallback((e) => {
-    e.preventDefault();
-
     setUserName(e.currentTarget.value)
   },[userName])
 
+  // 구글 로그인
+  const onGoogleLoginSuccess = useCallback((e) => {
+    if(channelName === ''){
+      return alert("Please enter the channelName");
+    }
+
+    dispatch(channelEnter(channelName))
+    dispatch(googleLogIn(e.profileObj.name))
+
+    const param = {
+      channelName: channelName,
+      userName: e.profileObj.name
+    };
+
+    axios.post('/api/save/roomUserName', param)
+      .then((res) => {
+        if(res.data.success === true){
+          routerCtx.history.push({ pathname: `/meeting` });
+        }
+      })
+  }, [userName, channelName])
+
+  // 일반 로그인
   const onEnterChanel = useCallback((e) => {
+    if(channelName === ''){
+      return alert("Please enter the channelName");
+    }
+    if(userName === ''){
+      return alert("Please enter the userName");
+    }
+
     dispatch(userLogIn(userName))
     dispatch(channelEnter(channelName))
 
-    routerCtx.history.push({ pathname: `/meeting` })
+    const param = {
+      channelName: channelName,
+      userName: userName
+    };
+
+    axios.post('/api/save/roomUserName', param)
+      .then((res) => {
+        if(res.data.success === true){
+          routerCtx.history.push({ pathname: `/meeting` });
+        }
+      })
   }, [userName, channelName])
 
   return (
@@ -76,17 +114,17 @@ export default function SignIn() {
             <div className="btn_container">
                 <div className="row">
                     <div id="Name_Your_Channel">
-                        <input id="Name_Your_Channel_input" placeholder="Name Your Channel" onChange={onChanelName} />
+                        <input id="Name_Your_Channel_input" placeholder="Name Your Channel" onChange={onChanelName} value={channelName} />
                     </div>
                     <div>
-                        <input id="Name_Your_Channel_input" placeholder="User Name" onChange={onUserName} />
+                        <input id="Name_Your_Channel_input" placeholder="User Name" onChange={onUserName} value={userName} />
                     </div>
                     <div id="Create_Channel">
                         <Button style={{ width: '35vw', height: '5vh' }} onClick={onEnterChanel}>Join Channel</Button>
                     </div>
                 </div>
 
-                <GoogleLoginForm />
+                <GoogleLoginForm onGoogleLoginSuccess={onGoogleLoginSuccess} />
             </div>
 
             <footer id="footer">
