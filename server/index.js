b08cab3774cab2b9bc92741d4d40b89d1bd4d592
park hyeong gil <gudrlf7913@naver.com>
2021-08-20 11:19:40 +0900

    라우터 분리

--- server/index.js
+++ server/index.js
@@ -1,11 +1,6 @@
-const firebase = require('firebase/app');
-const firebaseConfig = require('./config/firebaseConfig.js');
 const express = require('express');
-const router = express.Router();
 const bodyParder = require('body-parser');
-const redis = require('redis');
-
-require('firebase/firestore');
+const router = require('./router/index.js');
 
 const app = express();
 
@@ -14,64 +9,7 @@ app.use(bodyParder({
 }));
 app.use(bodyParder.json());
 
-//const client = redis.createClient(process.env.REDIS_URL);
-const client = redis.createClient();
-client.on("error", function(err, data){
-    if(err) throw err;
-})
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-/*
-doc을 채널 만든 사람이름으로 저장
-db.collection("RoomList").add({
-    roomName: "1",
-    roomPassword: "1234"
-}).then((e) => console.log("저장완료 : " + e))
-.catch((err) => console.log("저장 실패 : " + err))*/
-
-app.post('/api/save/roomUserName', (req, res) =>{
-    const bodyData = req.body;
-
-    client.sadd(bodyData.channelName,bodyData.userName);
-    return res.status(200).json({
-        success: true
-    })
-})
-
-app.post('/api/get/roomUserName', (req, res) => {
-    const bodyData = req.body;
-
-    client.smembers(bodyData.channelName, (error, items) => {
-        if(error){
-            return res.status(500).json({
-                success: false,
-                err: error
-            })
-        }
-
-        return res.status(200).json({
-            success: true,
-            userList: items
-        })
-    });
-})
-
-app.post('/api/remove/roomUserName', (req, res) => {
-    const bodyData = req.body;
-
-    if(client.srem(bodyData.channelName,bodyData.userName)){
-        return res.status(200).json({
-            success: true
-        })
-    }else{
-        return res.status(500).json({
-            success: false
-        })
-    }
-    
-})
+app.use('/api', router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>  console.log(`app listening on port ${port}!!`))
---
+++ server/router/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const redis = require('redis');
+const firebase = require('firebase/app');
+const firebaseConfig = require('../config/firebaseConfig.js');
+require('firebase/firestore');
+
+//const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient();
+client.on("error", function(err, data){
+    if(err) throw err;
+})
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+/*
+doc을 채널 만든 사람이름으로 저장
+db.collection("RoomList").add({
+    roomName: "1",
+    roomPassword: "1234"
+}).then((e) => console.log("저장완료 : " + e))
+.catch((err) => console.log("저장 실패 : " + err))*/
+
+router.post('/save/roomUserName', (req, res) =>{
+    const bodyData = req.body;
+
+    client.sadd(bodyData.channelName,bodyData.userName);
+    return res.status(200).json({
+        success: true
+    })
+})
+
+router.post('/get/roomUserName', (req, res) => {
+    const bodyData = req.body;
+
+    client.smembers(bodyData.channelName, (error, items) => {
+        if(error){
+            return res.status(500).json({
+                success: false,
+                err: error
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            userList: items
+        })
+    });
+})
+
+router.post('/remove/roomUserName', (req, res) => {
+    const bodyData = req.body;
+
+    if(client.srem(bodyData.channelName,bodyData.userName)){
+        return res.status(200).json({
+            success: true
+        })
+    }else{
+        return res.status(500).json({
+            success: false
+        })
+    }
+    
+})
+
+router.post('/room/register', (req, res) => {
+    const bodyData = req.body;
+
+    db.collection("RoomList").doc("test").set({
+        roomNumber: bodyData.roomNumber,
+        roomPassword: bodyData.roomPassword,
+        makeUserName: bodyData.makeUserName
+    })
+    .then((e) => {
+        return res.status(200).json({
+            success: true
+        })
+    })
+    .catch((err) => {
+        return res.status(500).json({
+            success: false,
+            err: err
+        });
+    })
+})
+
+module.exports = router;
