887317c926dc0fd11cb5fd4863a005067312f064
park hyeong gil <gudrlf7913@naver.com>
2021-08-20 18:11:05 +0900

    불필요 모듈 삭제

--- client/src/subcomponents/chatting.js
+++ client/src/subcomponents/chatting.js
@@ -1,13 +1,9 @@
 import React, { useMemo, useEffect, useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Fab from '@material-ui/core/Fab';
 import RTMClient from '../rtm-client';
 import ChattingUsersAndMessage from './chattingUsersAndMessage';
 import Dropzone from 'react-dropzone';
