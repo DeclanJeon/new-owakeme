b471ac19ac867b3332105005b6f2759ca81efdec
park hyeong gil <gudrlf7913@naver.com>
2021-08-23 14:25:11 +0900

    불필요한 search page 삭제

--- client/src/components/channelListPage/js/App.js
+++ client/src/components/channelListPage/js/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../css/App.css";
 import Header from "./Header";
-import SearchPage from "./SearchPage";
 import RecommendedVideos from "./RecommendedVideos";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -10,12 +9,7 @@ function App() {
     <div className="app">
       <Router>
         <Header />
-        <Switch>
-          <Route path="/search/:searchTerm">
-            <div className="app__page">
-              <SearchPage />
-            </div>
-          </Route>
+        <Switch> 
           <Route path="/">
             <div className="app__page">
               <RecommendedVideos />
