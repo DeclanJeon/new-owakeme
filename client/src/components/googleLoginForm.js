1c5aa1f88084aa687b63beac90d220f7fbdc358f
park hyeong gil <gudrlf7913@naver.com>
2021-08-26 13:57:31 +0900

    파이어베이스 구글 로그인 인증 연동

--- client/src/components/googleLoginForm.js
+++ client/src/components/googleLoginForm.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import FirebaseConfig from '../config/firebaseConfig';
+import FirebaseUiConfig from '../config/firebaseUiConfig';
+import { GoogleLogout } from 'react-google-login';
+import "../assets/css/google.css";
 
-const GoogleLoginForm = ({ onGoogleLoginSuccess }) => {
+const GoogleLoginForm = ({ setUserName }) => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+    // initializeApp 두번 호출하는 부분 조심하기
+    if (!firebase.apps.length) {
+        firebase.initializeApp(FirebaseConfig);
+    }else {
+        firebase.app(); // if already initialized, use that one
+    }
+    firebase.analytics();
+    
+    useEffect(() => {
+        if(isSignedIn){
+            console.log(firebase.auth().currentUser.photoURL)
+            setUserName(firebase.auth().currentUser.displayName);
+        }
+
+        const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+            setIsSignedIn(!!user);
+        });
+        return () => unregisterAuthObserver();
+    }, [isSignedIn])
+
+    if(!isSignedIn){
+        return (
+            <div>
+                <StyledFirebaseAuth uiConfig={FirebaseUiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+        )  
+    }
     return (
-        <>
-            <GoogleLogin 
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                onSuccess={onGoogleLoginSuccess}
-            />
-        </>
+        <div>
+            <GoogleLogout
+                className="googleLogout"
+                clientId={googleClientId}
+                buttonText="Sign out with Google"
+                onLogoutSuccess={() => firebase.auth().signOut()}
+            >
+            </GoogleLogout>
+        </div>
     )
 }
 
