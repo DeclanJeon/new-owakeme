e41a184676a5cb213f66866e1c3e3c012a4b374d
park hyeong gil <gudrlf7913@naver.com>
2021-08-26 14:39:36 +0900

    링크 공유시 작동될수 있도록 코드 수정

--- client/src/subcomponents/chattingUsersAndMessage.js
+++ client/src/subcomponents/chattingUsersAndMessage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+const DOMAIN_FORMAT = /(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi
+
 function chattingUsersAndMessage({location, userId, userMessage }) {
+    const regExp = new RegExp(DOMAIN_FORMAT, "i");
+
     return (
         <>
             <p className={`chat__message ${true && "chat__reciever"}`}>
                 <span className="chat__name">{userId}</span>
-                    {userMessage.message}
+                    {regExp.test(userMessage.message) ?
+                        <a href={userMessage.message}>{userMessage.message}</a>
+                        :
+                        userMessage.message
+                    }
                 <span className="chat__timestamp">{userMessage.messageTime}</span>
             </p>
         </>
