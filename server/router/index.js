863666d3c9b69c6813c83b9b9ac7ea027be1a5db
park hyeong gil <gudrlf7913@naver.com>
2021-09-13 17:41:56 +0900

    구글 사진 url 저장할수 있게 코드 수정

--- server/router/index.js
+++ server/router/index.js
@@ -5,11 +5,13 @@ const firebase = require('firebase/app');
 const firebaseConfig = require('../config/firebaseConfig.js');
 require('firebase/firestore');
 
-//const client = redis.createClient(process.env.REDIS_URL);
-const client = redis.createClient();
+const request = require('request');
+
+const client = redis.createClient(process.env.REDIS_URL);
+//const client = redis.createClient();
 client.on("error", function(err, data){
     if(err) throw err;
-});
+})
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -22,14 +24,20 @@ db.collection("RoomList").add({
 }).then((e) => console.log("저장완료 : " + e))
 .catch((err) => console.log("저장 실패 : " + err))*/
 
-router.post('/save/roomUserName', (req, res) =>{
-    const bodyData = req.body;
-
-    client.sadd(bodyData.channelName,bodyData.userName);
+router.get('/', (req, res) => {
     return res.status(200).json({
         success: true
     })
-});
+})
+
+router.post('/save/roomUserName', (req, res) =>{
+    const bodyData = req.body;
+   
+    client.sadd(bodyData.channelName, bodyData.userName);
+    return res.status(200).json({
+        success: true
+    })
+})
 
 router.post('/get/roomUserName', (req, res) => {
     const bodyData = req.body;
@@ -47,7 +55,7 @@ router.post('/get/roomUserName', (req, res) => {
             userList: items
         })
     });
-});
+})
 
 router.post('/remove/roomUserName', (req, res) => {
     const bodyData = req.body;
@@ -62,15 +70,17 @@ router.post('/remove/roomUserName', (req, res) => {
         })
     }
     
-});
+})
 
 router.post('/room/register', (req, res) => {
     const bodyData = req.body;
 
     db.collection("RoomList").add({
+        roomName: bodyData.roomName,
         roomNumber: bodyData.roomNumber,
         roomPassword: bodyData.roomPassword,
-        makeUserName: bodyData.makeUserName
+        makeUserName: bodyData.makeUserName,
+        photoURL: bodyData.photoURL
     })
     .then((e) => {
         return res.status(200).json({
@@ -83,7 +93,7 @@ router.post('/room/register', (req, res) => {
             error: err
         });
     })
-});
+})
 
 router.get('/room/roomList', (req, res) => {
     const roomArray = []
@@ -103,7 +113,7 @@ router.get('/room/roomList', (req, res) => {
             error: err
         })
     })
-});
+})
 
 router.post('/room/getRoomInfo', (req, res) => {
     const bodyData = req.body;
