f3b4d2da4fa7f51fca3a5fce06095d8ac1dcd288
park hyeong gil <gudrlf7913@naver.com>
2021-09-13 18:04:27 +0900

    디바이스 설정 코드 수정

--- client/src/subcomponents/settingDevice.js
+++ client/src/subcomponents/settingDevice.js
@@ -1,40 +1,90 @@
 import React, { useCallback, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import { Select, MenuItem, InputLabel, FormControl } from '@material-ui/core';
-import { setCameraId, setAudioId } from '../reducer/actions/deviceList';
+import { Select, MenuItem, InputLabel, FormControl, Button } from '@material-ui/core';
+import { setCameraId, setAudioId, setResolutionId } from '../reducer/actions/deviceList';
 
 const useStyle = makeStyles({
     deviceSection: {
         width: '30vw',
-        height: '100%'
+        height: '100%',
+        padding: '10px'
       },
+    menuTitle: {
+        color: '#333333',
+        textAlign: 'center',
+        fontSize: 'h6.fontSize',
+        position: 'relative',
+        top: '5px'
+      },
+    menu: {
+        margin: '0.4rem 0',
+        marginBottom: '50px',
+        position: 'relative',
+        height: '10px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
 })
 
-const SettingDevice = () => {
+const SettingDevice = ({ onUseSetting }) => {
     const classes = useStyle();
     const devices = useSelector(state => state.deviceReducer.deviceList);
-    const [audioDevices, setAudioDevices] = useState(devices.filter(device => device.kind == "audioinput"));
-    const [videoDevices, setVideoDevices] = useState(devices.filter(device => device.kind == "videoinput"));
-    debugger;
+
+    const audioDevices = devices.filter(device => device.kind == "audioinput");
+    const videoDevices = devices.filter(device => device.kind == "videoinput");
+
+    const [resolution, setResolution] = useState("480p");
+    const [audioDevice, setAudioDevice] = useState(audioDevices[0].deviceId);
+    const [videoDevice, setVideoDevice] = useState(videoDevices[0].deviceId);
+    
     const dispatch = useDispatch();
     
+    const selectResolution = useCallback((e) => {
+        setResolution(e.target.value);
+        dispatch(setResolutionId(e.target.value));
+    }, []);
+
     const selectVideo = useCallback((e) => {
-        dispatch(setCameraId(e.target.value))
+        setVideoDevice(e.target.value);
+        dispatch(setCameraId(e.target.value));
     }, [])
 
     const selectAudio = useCallback((e) => {
+        setAudioDevice(e.target.value);
         dispatch(setAudioId(e.target.value))
+    }, [])
+
+    const onApply = useCallback(() => {
+        onUseSetting();
     }, [])
 
     return (
         <>
             <div>
+                <FormControl className={classes.menu}>
+                    <span className={classes.menuTitle}>Setting</span>
+                </FormControl>
+
+                <FormControl className={classes.deviceSection}>
+                    <InputLabel>Resolution</InputLabel>
+                    <Select
+                        onChange={selectResolution}
+                        value={resolution}
+                    >
+                        <MenuItem value="480p">480p</MenuItem>
+                        <MenuItem value="720p">720p</MenuItem>
+                        <MenuItem value="1080p">1080p</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
                 <FormControl className={classes.deviceSection}>
                     <InputLabel>Video</InputLabel>
                     <Select
                         onChange={selectVideo}
-                        value={videoDevices[0].deviceId}
+                        value={videoDevice}
                     >
                         {videoDevices.map((obj) => {
                             return <MenuItem value={obj.deviceId} key={obj.deviceId} >{obj.label}</MenuItem>
@@ -47,7 +97,7 @@ const SettingDevice = () => {
                     <InputLabel>Audio</InputLabel>
                     <Select
                         onChange={selectAudio}
-                        value={audioDevices[0].deviceId}
+                        value={audioDevice}
                     >
                         {audioDevices.map((obj) => {
                             return <MenuItem value={obj.deviceId} key={obj.deviceId} >{obj.label}</MenuItem>
@@ -55,6 +105,9 @@ const SettingDevice = () => {
                     </Select>
                 </FormControl>
             </div>
+            <Button variant="contained" color="primary" onClick={onApply}>
+                Apply
+            </Button>
         </>
     )
 }
