bce1ef88f4af573cc3bd59c32641f691bb799cba
park hyeong gil <gudrlf7913@naver.com>
2021-09-13 17:47:36 +0900

    roomList 파일 경로 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import "./App.css";
 import { BrowserRouterHook } from "./utils/use-router";
 import { Switch, Route } from "react-router-dom";
 import Index from "./pages/index";
 import Meeting from "./pages/meeting";
 import ShareScreen from "./pages/shareScreen";
-import ChannelList from "./components/channelListPage/js/App";
+import RoomList from "./pages/roomList";
 
 function App() {
   return (
     <BrowserRouterHook>
       <Switch>
-        <Route exact path="/channelList" component={ChannelList}></Route>
+        <Route exact path="/roomList" component={RoomList}></Route>
         <Route exact path="/shareScreen" component={ShareScreen}></Route>
         <Route exact path="/meeting" component={Meeting}></Route>
         <Route path="/" component={Index}></Route>
