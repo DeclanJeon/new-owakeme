7397d062cd5edb406c265ad5d674986b08cb43ef
park hyeong gil <gudrlf7913@naver.com>
2021-09-13 18:05:36 +0900

    방 리스트 컴포넌트 추가

---
+++ client/src/components/roomCard.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from "react";
+import axios from 'axios';
+import useRouter from '../utils/use-router';
+
+
+function RoomCard({ roomName, roomNumber, makeUserName, photoURL }) {
+  const routerCtx = useRouter();
+  const [enterPassword, setEnterPassword] = useState('');
+  
+  const onEnterRoom = useCallback((roomName) => {
+    //비밀번호 확인
+    const body = {
+      channelName: roomNumber,
+      roomPassword: enterPassword
+    }
+    axios.post('/api/room/getRoomInfo', body).then(res => {
+      if (res.data.passwordCheckResult) {
+        routerCtx.history.push({ pathname: `/meeting` });
+      }else{
+        alert(res.data.error);
+        setEnterPassword('');
+      }
+    })
+  }, [enterPassword]);
+
+  const onChangeEnterPassword = useCallback((e) => {
+    setEnterPassword(e.target.value);
+  }, []);
+
+  return (
+    <>
+      <div className="channel__box">
+        <div className="channel__inner__box">
+          <div className="profile"><img src={photoURL} /></div>
+          <div className="makeUserName">{makeUserName}</div>
+          <div className="room__number"><input type="text" value={roomName} disabled="true" /></div>
+          <div className="room__name"><input type="text" placeholder="Room Name" value={roomNumber} disabled="true" /></div>
+          <div className="room__password"><input type="password" placeholder="Password" value={enterPassword} onChange={onChangeEnterPassword} /></div>
+          <div className="room_button"><button onClick={onEnterRoom}>Enter</button></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RoomCard
---
+++ client/src/pages/roomList.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../assets/css/roomList.css';
+import RoomCard from '../components/roomCard';
+import new_owake_logo_white from "../assets/img/new_owake_logo_white.svg";
+
+function RoomList() {
+  const [roomList, setRoomList] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/room/roomList').then(res => {
+      setRoomList(res.data.roomList);
+    })
+  }, [])
+
+  return (
+    <>
+      <div className="container">
+        <div className="inner__container">
+          <div className="header__container">
+            <div className="logo">
+              <img src={new_owake_logo_white} alt="" />
+            </div>
+          </div>
+
+          <div className="body__container">
+            <div className="body__channel__container">
+              {
+                 roomList.map((obj, index) => {
+                   return (
+                    <RoomCard 
+                      roomName={obj.roomName}
+                      roomNumber={obj.roomNumber}
+                      makeUserName={obj.makeUserName}
+                      photoURL={obj.photoURL}
+                      key={index}
+                    />
+                   )
+                 })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default RoomList
