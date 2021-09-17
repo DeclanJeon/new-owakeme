7294c300d9bb3a8406c6489498012094758d2d49
park hyeong gil <gudrlf7913@naver.com>
2021-09-17 11:36:22 +0900

    track 리듀서 코드 추가

--- client/src/reducer/actions/track.js
+++ client/src/reducer/actions/track.js
@@ -1,5 +1,9 @@
-export const SET_LOCAL_TRACK = 'SET_LOCAL_TRACK'
-export const SET_LEAVE_LOCAL_TRACK = 'SET_LEAVE_LOCAL_TRACK'
+export const SET_LOCAL_TRACK = 'SET_LOCAL_TRACK';
+export const SET_LEAVE_LOCAL_TRACK = 'SET_LEAVE_LOCAL_TRACK';
+export const SET_ON_LOCAL_SHARE = 'SET_ON_LOCAL_SHARE';
+export const SET_OFF_LOCAL_SHARE = 'SET_OFF_LOCAL_SHARE';
+export const SET_USE_SHARING = 'SET_USE_SHARING';
+export const SET_DONT_USE_SHARING = 'SET_DONT_USE_SHARING';
 
 export const onLocalTrack = (videoTrack, audioTrack, client) => {
     return {
@@ -16,3 +20,28 @@ export const onLeaveLocalTrack = () => {
     }
 }
 
+export const onLocalShare = (shareClient, shareTrack) => {
+    return {
+        type: SET_ON_LOCAL_SHARE,
+        shareClient: shareClient,
+        shareTrack: shareTrack
+    }
+}
+
+export const offLocalShare = () => {
+    return {
+        type: SET_OFF_LOCAL_SHARE
+    }
+}
+
+export const useSharing = () => {
+    return {
+        type: SET_USE_SHARING
+    }
+}
+
+export const dontUseSharing = () => {
+    return {
+        type: SET_DONT_USE_SHARING
+    }
+}
--- client/src/reducer/reducers/track.js
+++ client/src/reducer/reducers/track.js
@@ -1,9 +1,12 @@
-import { SET_LOCAL_TRACK, SET_LEAVE_LOCAL_TRACK } from '../actions/track'
+import { SET_LOCAL_TRACK, SET_LEAVE_LOCAL_TRACK, SET_ON_LOCAL_SHARE, SET_OFF_LOCAL_SHARE, SET_USE_SHARING, SET_DONT_USE_SHARING } from '../actions/track'
 
 const initState = {
     localVideo: undefined,
     localAudio: undefined,
     localClient: undefined,
+    shareClient: undefined,
+    shareTrack: undefined,
+    sharing: true,
     isLocal: false,
 }
 
@@ -25,8 +28,30 @@ const trackReducer = (state = initState, action) => {
                 localClient: undefined,
                 isLocal: false
             }
+        case SET_ON_LOCAL_SHARE:
+            return {
+                ...state,
+                shareClient: action.shareClient,
+                shareTrack: action.shareTrack
+            }
+        case SET_OFF_LOCAL_SHARE:
+            return {
+                ...state,
+                shareClient: undefined,
+                shareTrack: undefined
+            }
+        case SET_USE_SHARING:
+            return {
+                ...state,
+                sharing: true
+            }
+        case SET_DONT_USE_SHARING:
+            return {
+                ...state,
+                sharing: false
+            }
         default:
-            return state;
+            return state
     }
 }
 
